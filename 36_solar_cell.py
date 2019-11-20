b6e611a4d5315d31039e37f81203bf330cc2355c
Chika2020 <chikaodiri.nwachukwu@gmail.com>
2019-11-20 18:48:59 +0000

    modified:   18_thermistor.py 	modified:   36_solar_cell.py 	new file:   emonTX.py 	new file:   insolarxtemp.py

--- 36_solar_cell.py
+++ 36_solar_cell.py
@@ -6,6 +6,13 @@ import math
 
 DO = 17
 GPIO.setmode(GPIO.BCM)
+def voltageMon():
+	while(1):
+		aVal = ADC.read(1)
+		V = 5 * float(aVal) / 255
+                print("Voltage:", V)
+
+
 
 def setup():
         ADC.setup(0x48)
@@ -15,19 +22,20 @@ def setup():
 def loop():
         status = 1
         tmp = 1
-        analogVal = ADC.read(2)
-        Vr = 5 * float(analogVal) / 255
-
+        analogVal = ADC.read(1)
+        Vr = 5 * float(analogVal) / 255 # Should be 5V -Yeray
+	
        # analogVal = ADC.read(1)
         #Vr = 5 * float(analogVal) / 255
         Area = 50.8* 50.8 / (100*100)
         Power = pow(Vr, 2) /465
         Radiation = Power / Area
-        print(Vr)
-        print(Radiation)
+        print("Vr",Vr)
+        print("Rad",Radiation)
 
 
 if __name__ == '__main__':
-                setup()
+		setup()
+		#voltageMon()
                 loop()
 
--- insolarxtemp.py
+++ insolarxtemp.py
@@ -19,23 +19,37 @@ def setup():
 	GPIO.setup(DO, GPIO.IN)
 
 	
-def loop():
+def temp():
 	status = 1
 	tmp = 1
 	
 	analogVal = ADC.read(0) #should be ground -Yeray
 	Vr = 5 * float(analogVal) / 255
-	print("Voltage 2",Vr)
 	Rt = 10000 * Vr / (5 - Vr)
 	temp = 1/(((math.log(Rt / 10000)) / 3950) + (1 / (273.15+25)))
 	temp = temp - 273.15
 	print ('temperature', temp)
-        print ('voltage 2',Vr)
+        print ('voltage temp',Vr)
+
+
+def insolar():
+        status = 1
+        tmp = 1
+        analogVal = ADC.read(1)
+        Vr = 5 * float(analogVal) / 255 # Should be 5V -Yeray
+
+        Area = 50.8* 50.8 / (100*100)
+        Power = pow(Vr, 2) /465
+        Radiation = Power / Area
+        print("solar cell voltage",Vr)
+        print ("Insolation", Radiation)
+
 
 if __name__ == '__main__':
 	try:
 		setup()
 		#voltageMon()
-		loop()
+		temp()
+                insolar()
 	except KeyboardInterrupt: 
 		pass	
